5f24485ccf15c839c48c264a5fab620a631de5e2
Maxip32 <piolattoemanuel@gmail.com>
2022-09-25 16:01:29 -0300

    work 03

---
+++ 03-JS-II/homework/TareaTeorica2.js
@@ -0,0 +1,9 @@
+for: el software se ejecuta en bucles evaluando expresiones hasta que devuelve lo que estamos buscando o se detiene despues de cierto tiempo.
+
+Operadores logicos (&&; ||; !): 
+
+El primero "Y" (&&) evalúa ambas expresiones, si son verdaderas devuelve true; si alguna no lo es, devuelve false. 
+
+El segundo "O" (||) devolverá true si al menos una de las expresiones comparadas es verdadera, y false si todas son falsas. 
+
+El tercero "NO" (!) devolverá true si la expresion es falsa, y false si la expresion es verdadera. 
